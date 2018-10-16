c7918cf92e5159c0a4fb4af57178cc9ea41fcfa7
jojo <fujiao@100tal.com>
2018-10-16 16:16:06 +0800

    首页头部

--- src/components/home/homeHeader.vue
+++ src/components/home/homeHeader.vue
@@ -1,5 +1,46 @@
 <template>
     <div class="home-header">
-        <div></div>
+        <i class="logo"></i>
+        <div class="search">
+            <ul>
+                <li>周润发捐58亿</li>
+                <li>阚清子恋情</li>
+                <li>霍金遗作发售</li>
+                <li>崔永元真面饭馆</li>
+                <li>周润发捐58亿</li>
+            </ul>
+        </div>
+        <!-- <i class="live-broadcast"></i> -->
+        <span class="live-broadcast">直播</span>
     </div>
 </template>
+
+<style scoped>
+.home-header{
+    display: flex;
+    align-items: center;
+    height: .6rem;
+    padding: .1rem .2rem;
+    background-color: #ee1a1a;
+    color: #fff;
+    font-size: .2rem;
+}
+.logo{
+    width: .6rem;
+    height: .4rem;
+    background: url('../../assets/imgs/logo.png') center no-repeat;
+}
+.search{
+    flex: auto;
+    background-color: #e96767d8;
+    height: .6rem;
+    margin: 0 .2rem;
+    border-radius: .3rem;
+}
+.search li{
+    height: .6rem;
+}
+.live-broadcast{
+    width: .5rem;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import './assets/styles/reset.css'
 import './utils/viewPort.js'
 
 Vue.config.productionTip = false
