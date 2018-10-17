54aa20876fa0293fda02e4a86ca97b1a9c6ef072
jojo <fujiao@100tal.com>
2018-10-17 17:03:40 +0800

    小改动

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,20 +1,31 @@
 <template>
   <div class="hello">
     <homeHeader></homeHeader>
+    <homeTopping :topping-list="toppingList"></homeTopping>
     <footTab></footTab>
   </div>
 </template>
 
 <script>
 import homeHeader from './home/homeHeader.vue'
+import homeTopping from './home/homeTopping.vue'
 import footTab from './footTab.vue'
 
 export default {
   name: 'home',
-  components: {homeHeader,footTab},
+  components: {homeHeader,footTab,homeTopping},
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      toppingList: [
+        {
+          title: '重阳节，重温习近平的这些暖人瞬间',
+          site: '央视新闻'
+        },
+        {
+          title: '二十多年，习近平一直牢记着这句话',
+          site: '新华网'
+        }
+      ]
     }
   }
 }
---
+++ src/components/home/homeTopping.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="home-topping">
+        <article v-for="item in toppingList">
+            <div class="title">{{item.title}}</div>
+            <div class="info">
+                <span class="topping">置顶</span>
+                <span class="site">{{item.site}}</span>
+            </div>
+        </article>
+    </div>
+</template>
+
+<script>
+	export default{
+		name: 'homeTopping',
+		props: ['toppingList']
+	}
+</script>
+
+<style scoped>
+article{
+    border-bottom: 1px solid rgb(221, 217, 217);
+}
+.title{
+    font-size: .3rem;
+}
+.info{
+    font-size: .15rem;
+}
+.topping{
+    color: #ee1a1a;
+}
+.site{
+    color: #ccc;
+}
+</style>
